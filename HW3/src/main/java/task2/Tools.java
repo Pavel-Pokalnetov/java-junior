ba811b0039c8c7fd77eaaf0a433aae3f79c2033c
Pavel-Pokalnetov <redx@mail.ru>
2023-12-07 18:13:24 +0500

    task2 - готово

---
+++ HW3/src/main/java/task2/Tools.java
@@ -0,0 +1,28 @@
+package task2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Tools {
+    public static void saveToFile(String string, String filename) throws IOException {
+        try (FileWriter fw = new FileWriter(filename)) {
+            fw.write(string);
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+    }
+
+    public static String readFromFile(String filename) throws IOException {
+        String xmlStudent;
+        try {
+            xmlStudent = Files.readString(Paths.get(filename), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+        return xmlStudent;
+    }
+
+}
